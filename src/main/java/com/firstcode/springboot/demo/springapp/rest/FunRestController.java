caa05b004be8303f72049998725675b0075f511f
firstzagalaxy <first.chetsadakorn@gmail.com>
2024-03-17 21:28:59 +0700

    update files from git2

--- src/main/java/com/firstcode/springboot/demo/springapp/rest/FunRestController.java
+++ src/main/java/com/firstcode/springboot/demo/springapp/rest/FunRestController.java
@@ -1,10 +1,24 @@
 package com.firstcode.springboot.demo.springapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    //inject properties for : coach.name and team.name
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    //expose new endpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
     // expose "/" that return "Hello World"
     @GetMapping("/")
     public String sayHello(){
@@ -24,4 +38,4 @@ public class FunRestController {
     public String getDailyFortune(){
         return "Today is your lucky day";
     }
-}//test
+}//test2
