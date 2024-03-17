3b9a0ea3e36715e3cee00d18137ddc2ed69ccb08
firstzagalaxy <first.chetsadakorn@gmail.com>
2024-03-17 22:26:41 +0700

    constructor-injection2

---
+++ src/main/java/com/firstcode/springboot/DemoinjectionApplication.java
@@ -0,0 +1,13 @@
+package com.firstcode.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoinjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoinjectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firstcode/springboot/DemoinjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstcode.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoinjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
