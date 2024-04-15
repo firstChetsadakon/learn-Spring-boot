14b0455966e1f459900e285230379c3a004f4974
firstzagalaxy <first.chetsadakorn@gmail.com>
2024-04-15 10:21:33 +0700

    //define a constructor for dependency injection     //if it's having 1 injection this is optional for the constructor, but it is a good practice to use it     //but i will keep it for now to be explicit about the dependency injection if it is one injection only

--- src/main/java/com/firstcode/springboot/DemoController.java
+++ src/main/java/com/firstcode/springboot/DemoController.java
@@ -11,9 +11,8 @@ public class DemoController {
     private Coach myCoach;
 
     //define a constructor for dependency injection
-    //if it's having 1 injection this is optional for the constructor, but it is a good practice to use it
-    //but i will keep it for now to be explicit about the dependency injection if it is one injection only
-    @Autowired
+    @Autowired //if it's having 1 injection this is optional for the constructor, but it is a good practice to use it
+    // but i will keep it for now to be explicit about the dependency injection if it is one injection only
     DemoController(Coach theCoach) {
         myCoach = theCoach;
     }
