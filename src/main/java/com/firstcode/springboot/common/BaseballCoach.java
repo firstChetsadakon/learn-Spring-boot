161df5adafd0c96ae64217c4ca9481b11ab70137
firstzagalaxy <first.chetsadakorn@gmail.com>
2024-04-15 14:08:20 +0700

    @Qualifier for tell spring what bean  have to pick up for run the application

---
+++ src/main/java/com/firstcode/springboot/common/BaseballCoach.java
@@ -0,0 +1,11 @@
+package com.firstcode.springboot.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Practice hitting for 30 minutes";
+    }
+}
--- src/main/java/com/firstcode/springboot/rest/DemoController.java
+++ src/main/java/com/firstcode/springboot/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.firstcode.springboot.rest;
 
 import com.firstcode.springboot.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public void setCoach(Coach theCoach) {
+    public DemoController(@Qualifier("trackCoach")Coach theCoach) {
         myCoach = theCoach;
     }
 
