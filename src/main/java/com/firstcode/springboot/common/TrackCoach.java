bd409e640f0a684a687567ecc96bfc84aa0e1119
firstzagalaxy <first.chetsadakorn@gmail.com>
2024-04-15 17:22:26 +0700

    used Primary in TrackCoach for tell Spring what bean to create instance

--- src/main/java/com/firstcode/springboot/common/TrackCoach.java
+++ src/main/java/com/firstcode/springboot/common/TrackCoach.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Component;
 public class TrackCoach implements Coach{
     @Override
     public String getDailyWorkout() {
-        return "Run a hard 5k!";
+        return "Run a hard 5k!!";
     }
 }
