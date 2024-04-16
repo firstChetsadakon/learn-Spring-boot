ac364c386f0691133b46d35d7a444b993849ef03
firstzagalaxy <first.chetsadakorn@gmail.com>
2024-04-16 11:30:38 +0700

    test lazy initialization for application

--- src/main/java/com/firstcode/springboot/common/BaseballCoach.java
+++ src/main/java/com/firstcode/springboot/common/BaseballCoach.java
@@ -5,6 +5,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class BaseballCoach implements Coach{
+
+    public BaseballCoach() {
+        System.out.println("in constructor : " + getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Practice hitting for 30 minutes";
--- src/main/java/com/firstcode/springboot/common/TennisCoach.java
+++ src/main/java/com/firstcode/springboot/common/TennisCoach.java
@@ -1,9 +1,15 @@
 package com.firstcode.springboot.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach{
+
+    public TennisCoach() {
+        System.out.println("in constructor : " + getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Practice your backhand volley";
