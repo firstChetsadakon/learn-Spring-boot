46a0104bcc7b8a410d8758b1c093763aa0d7923b
firstzagalaxy <first.chetsadakorn@gmail.com>
2024-05-01 23:23:25 +0700

    added List Student and use PostConstruct for load data of student

---
+++ src/main/java/com/firstcode/demo/rest/StudentRestController.java
@@ -0,0 +1,47 @@
+package com.firstcode.demo.rest;
+
+import com.firstcode.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api")
+public class StudentRestController {
+
+    private List<Student> theStudents;
+
+    // define !PostConstructor to load the student data ... only once!
+
+    @PostConstruct
+    public void loadData() {
+
+        theStudents = new ArrayList<>();
+        theStudents.add(new Student("Poornima", "Patel"));
+        theStudents.add(new Student("Mario", "Rossi"));
+        theStudents.add(new Student("Mary", "Smith"));
+    }
+
+    // define endpoint for "/student" = return a list of students
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return theStudents;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
