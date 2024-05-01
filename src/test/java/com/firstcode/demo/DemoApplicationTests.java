01d672e060ff5c5120ab0c8fa8fd140eacc728f9
firstzagalaxy <first.chetsadakorn@gmail.com>
2024-05-01 20:29:38 +0700

    new demo project spring-boot-rest-crud - first commit

--- src/test/java/com/firstcode/demo/DemoApplicationTests.java
+++ src/test/java/com/firstcode/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.firstcode.cruddemo;
+package com.firstcode.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CruddemoApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
