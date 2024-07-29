be935ad9f4728a985eb1ca709499a69f2240765f
firstzagalaxy <first.chetsadakorn@gmail.com>
2024-07-29 16:14:09 +0700

    change path fpr entity employees to members  - spring can handle with entity to expose resource path wit pluralized form BUT can't handle with some word  - use config to more readable and to config specific detail for our resource

--- src/main/java/com/first/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/first/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.first.springboot.cruddemo.dao;
 
 import com.first.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // แค่นี้เลยสำหรับ repository
 }
