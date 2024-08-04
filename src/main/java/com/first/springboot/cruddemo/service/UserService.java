b2c6d9984047aa1bcfd9b27d509771ceaafb9ddf
firstzagalaxy <first.chetsadakorn@gmail.com>
2024-08-04 22:46:15 +0700

    - UserService  - added findByUsername  - added loadUserByUsername  - added mapRolesToAuthorities

---
+++ src/main/java/com/first/springboot/cruddemo/service/UserService.java
@@ -0,0 +1,8 @@
+package com.first.springboot.cruddemo.service;
+
+import com.first.springboot.cruddemo.entity.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+    public User findByUsername(String userName);
+}
